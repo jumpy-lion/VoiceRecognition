ef698bf11e2d3c9f6faa387c846bdb02918e9077
ktro <karolt2001@gmail.com>
2024-03-28 21:19:29 +0100

    Remove FFMpegCore Dependency and Simplify Processing Pipeline
    Removed the dependency on FFMpegCore, modifying the microphone reader to directly generate wav data. This allows for direct processing of the raw recording data without requiring a separate conversion step, simplifying the processing pipeline. FfmpegWrapper.cs was removed as it's no longer necessary, and changes were made accordingly in MainProgram.cs and other related files. These modifications should enhance performance and reduce complexity.

--- MicrophoneReader.cs
+++ MicrophoneReader.cs
@@ -1,5 +1,4 @@
-using FFMpegCore.Extend;
-using FFMpegCore.Pipes;
+using System.Text;
 using OpenTK.Audio.OpenAL;
 
 namespace VoiceRecognition;
@@ -7,29 +6,57 @@ namespace VoiceRecognition;
 public class MicrophoneReader
 {
     public bool ShouldRun { get; set; } = true;
-
     private const int SampleRate = 16000;
     private const ALFormat Format = ALFormat.Mono16;
-    private const int BufferSize = SampleRate/2;
-    private FileStream Output { get; }
+    private const int BufferSize = SampleRate / 2;
+    private static readonly char[] WavHeaderRiff = ['R', 'I', 'F', 'F'];
+    private static readonly char[] WavHeaderWave = ['W', 'A', 'V', 'E'];
+    private static readonly char[] WavHeaderFmt = ['f', 'm', 't', ' '];
+    private static readonly char[] WavHeaderData = ['d', 'a', 't', 'a'];
 
-    public MicrophoneReader(string destination)
+    public void Start(Stream output)
     {
-        Output = File.Open(destination, FileMode.Create);
+        var intermediateStream = new MemoryStream();
         var thread = new Thread(() =>
         {
             var device = ALC.CaptureOpenDevice(null, SampleRate, Format, BufferSize);
             ALC.CaptureStart(device);
             var buffer = new byte[SampleRate];
+            var totalSampleCount = 0;
             while (ShouldRun)
             {
                 var samples = ALC.GetInteger(device, AlcGetInteger.CaptureSamples);
                 ALC.CaptureSamples(device, buffer, samples);
-                Output.Write(buffer, 0, samples*sizeof(short));
+                intermediateStream.Write(buffer, 0, samples * sizeof(short));
+                totalSampleCount += samples;
             }
+
             ALC.CaptureStop(device);
             ALC.CaptureCloseDevice(device);
+            WriteWavHeader(output, 1, totalSampleCount);
+            intermediateStream.Position = 0;
+            intermediateStream.CopyTo(output);
         });
         thread.Start();
+    }
+
+    private static void WriteWavHeader(Stream stream, ushort channelCount, int totalSampleCount)
+    {
+        const ushort bitDepth = 16;
+
+        using var writer = new BinaryWriter(stream, Encoding.UTF8, true);
+        writer.Write(WavHeaderRiff);
+        writer.Write(44 + totalSampleCount * bitDepth / 8);
+        writer.Write(WavHeaderWave);
+        writer.Write(WavHeaderFmt);
+        writer.Write(16);
+        writer.Write((ushort)1);
+        writer.Write(channelCount);
+        writer.Write(SampleRate);
+        writer.Write(SampleRate * channelCount * bitDepth / 8);
+        writer.Write((ushort)(channelCount * bitDepth / 8));
+        writer.Write(bitDepth);
+        writer.Write(WavHeaderData);
+        writer.Write(totalSampleCount * bitDepth / 8);
     }
 }
